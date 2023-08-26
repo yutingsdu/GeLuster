9281099228ee50fcec011c6b9b4c707cb18e791d
yutingsdu <yutingsdu@163.com>
2023-08-26 15:24:33 +0800

    Add files via upload

--- sample_test/run_me.sh
+++ sample_test/run_me.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+######################## Set the environment variables ########################
+#
+#export PATH=/path_to_minimap2/:$PATH
+#export PATH=/path_to_samtools/:$PATH
+
+../GeLuster -r Test.fastq -f fq -s cDNA -o geluster_outdir
