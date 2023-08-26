0919345d95d5a5fb247d4940e2b2ded173c8dc3c
yutingsdu <yutingsdu@163.com>
2023-08-26 16:45:48 +0800

    update run_me.sh

--- sample_test/run_me.sh
+++ sample_test/run_me.sh
@@ -2,7 +2,12 @@
 
 ######################## Set the environment variables ########################
 #
-#export PATH=/path_to_minimap2/:$PATH
-#export PATH=/path_to_samtools/:$PATH
+# export PATH=/path_to_minimap2/:$PATH
+# export PATH=/path_to_samtools/:$PATH
+#
+# e.g.
+# export PATH=/home/yuting/yuting/Software/minimap2-2.24/:$PATH
+# export PATH=/home/yuting/yuting/Software/samtools-1.10/:$PATH
+#
 
 ../GeLuster -r Test.fastq -f fq -s cDNA -o geluster_outdir
